b1e30465dfcef2c9105a4effd19a57e5a1142776
Olivier Coué <olivier28.coue@gmail.com>
2016-07-25 18:03:49 +0200

    client side: fix iv-auth.service imports

--- src/app/iv-auth/iv-auth.guard.ts
+++ src/app/iv-auth/iv-auth.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, Observable }             from '@angular/core';
+import { Injectable }             from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot }    from '@angular/router';
 import { IvAuthService }        from './iv-auth.service';
 
