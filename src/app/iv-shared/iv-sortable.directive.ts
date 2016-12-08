5f25fdd8d4a896787353e9d22662f9464e9202cf
Olivier Coué <olivier28.coue@gmail.com>
2016-12-08 22:59:15 +0100

    client side: add class to moving item on sortable

--- src/app/iv-shared/iv-sortable.directive.ts
+++ src/app/iv-shared/iv-sortable.directive.ts
@@ -13,9 +13,12 @@ export class IvSortableDirective {
 
     constructor(el: ElementRef, private collectionService: IvCollectionService) {
         this.el = el.nativeElement;
+        var elCopy = this.el;
         let newIndex;
         let oldIndex;
         $(this.el).sortable({
+            placeholder: "card-ghost col-xs-12 col-md-6 col-lg-4 col-xl-3",
+            helper: function(x, y){ y.addClass('card-moving'); return y.context },
             handle: '.move-item-button',
             cancel: '.cancel-sort',
             start: (event, ui) => {
@@ -27,6 +30,7 @@ export class IvSortableDirective {
                 $(this).removeAttr('data-previndex');
             },
             stop: (event, ui) => {
+                ui.item.removeClass('card-moving');
                 let tmpItem = this.list[oldIndex];
                 if(!tmpItem)
                     return;
