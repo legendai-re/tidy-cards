7baf55547c484683413837106b5a0342f58c64aa
Olivier Coué <olivier28.coue@gmail.com>
2016-12-11 00:23:43 +0100

    server side: fix logout

--- server/routes/authRouter/getLogout.js
+++ server/routes/authRouter/getLogout.js
@@ -1,4 +1,6 @@
 module.exports = function getLogout(req, res) {
+    req.session.cookie.maxAge = 0;
 	req.logout();
+    req.session.destroy();
     res.json({'success': true, 'alert': 'User loged out'});
 }
