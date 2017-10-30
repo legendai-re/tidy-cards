f502997c18d61bc646250cf8abbfa064f89a6c5e
OlivierCoue <olivier28.coue@gmail.com>
2017-10-30 23:22:11 +0100

    server side: handle image proxy pipe errors

--- server/routes/imageRouter/getOne.js
+++ server/routes/imageRouter/getOne.js
@@ -1,8 +1,16 @@
 module.exports = function getOne (req, res) {
 	var request = require('request');
 
-	if(req.query && req.query.url)
-    	request.get(req.query.url).pipe(res);
-    else
+	if(req.query && req.query.url){
+		var stream = request.get(req.query.url);
+		stream.on('error', function(e){
+			res.status(400).json({error: "Bad url params"});
+		});
+
+		stream.pipe(res).on('error', function(e){
+			res.status(400).json({error: "Bad url params"});
+		});
+	}else{
     	res.sendStatus(400);
+	}
 }
